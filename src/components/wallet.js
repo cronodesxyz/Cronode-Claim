e5cbff54cbe51f8c5591345c31bc4a46aaac1b95
king-of-the-verse <al7205049@gmail.com>
2021-11-22 13:09:38 -0600

    initial

--- src/components/wallet.js
+++ src/components/wallet.js
@@ -7,6 +7,13 @@ export const Wallet = () => {
 
     const handleActiveSubmit = () => {
         activateBrowserWallet()
+        const main = document.getElementById("main");
+
+
+    const totalsupply = fetch('https://deep-index.moralis.io/api/v2/nft/0xB80a06EA0f4D17DD7D4b584DAA483C760331137B?chain=eth&format=decimal&order=DESC',
+     {headers: {'X-API-Key': 'T7d37JqEbexjZRNh2S1uuDRmEvW6f7hmipNPyKrp6P4paJU6G35vuecqMHR0K21A'}})
+    totalsupply.then((response) => {return response.json();}).then((total) => {const value = total.total; main.innerHTML = value;});
+
 
     }
 
