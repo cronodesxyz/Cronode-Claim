589e47b4188023e5793970641e514d4d57d35422
king-of-the-verse <al7205049@gmail.com>
2022-01-03 19:26:58 -0600

    intial

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './css/styles.css';
 import './css/App.css';
 import {Wallet} from "./components/wallet";
 import {BuyForm} from "./components/mint";
-import logo from "./assets/small-logo.png";
+import logo from "./assets/LH.png";
 
 
 function App() {
@@ -16,11 +16,11 @@ function App() {
 
 
             <div className="div-block-83 outer">
-                                <a href="#header" className="link-block-7 w-inline-block  w--current"><img className="logo-image" src={logo} loading="lazy" width="130" height="75" alt="hi"/></a>
+
 
                 <Wallet/>
                 <h1 _ngcontent-ivp-c18=""  className="heading-78-copy-copy"><span id="main"></span><span>/10000</span></h1>
-
+                <a href="#header" className="link-block-7 w-inline-block  w--current"><img className="logo-image" src={logo} width="130" height="75"  alt="LH"/></a>
 
                 <app-root _nghost-ivp-c18="" ng-version="9.0.7"><div _ngcontent-ivp-c18="" className="div-block-83 inner">
                 <h1 _ngcontent-ivp-c18="" className="heading-78-copy">mint a hustla</h1>
